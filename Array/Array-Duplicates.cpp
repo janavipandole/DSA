ecc22e0bcc9204fe78f0e5325f2021289d0df9d4
Janavi Pandole <janavipandole@gmail.com>
2025-08-28 07:00:27 +0530

    Array Duplicates

---
+++ Array/Array-Duplicates.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+vector<int> findDuplicates(vector<int> &nums)
+{
+    unordered_map<int, int> mpp;
+    vector<int> ans;
+    for (auto num : nums)
+    {
+        mpp[num]++;
+    }
+
+    for (auto(val) : mpp)
+    {
+        if (val.second == 2)
+        {
+            ans.push_back(val.first);
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {2, 3, 1, 2, 3};
+    vector<int> ans = findDuplicates(nums);
+    cout << "findDuplicates : ";
+    for (auto num : ans)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
