8dc92f91902df4534c5b82b862384bb73948e85a
Janavi Pandole <janavipandole@gmail.com>
2025-08-30 13:14:26 +0530

    Median of two Sorted Arrays of Different Sizes Optiaml solution

--- BinarySearch/Median-of-Two-Sorted-Arrays.cpp
+++ BinarySearch/Median-of-Two-Sorted-Arrays.cpp
@@ -40,7 +40,7 @@ double findMedianSortedArraysBrute(vector<int> &nums1, vector<int> &nums2)
 
     return ans;
 }
-// Better force solution
+// Better solution
 double findMedianSortedArraysBetter(vector<int> &nums1, vector<int> &nums2)
 {
     int i = 0, j = 0;
@@ -117,12 +117,48 @@ double findMedianSortedArraysBetter(vector<int> &nums1, vector<int> &nums2)
     return ans;
 }
 
+// Optimal solution
+double findMedianSortedArraysOptimal(vector<int> &nums1, vector<int> &nums2)
+{
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+    if (n1 > n2) findMedianSortedArraysOptimal(nums2, nums1); // Always try to tak first as smalest array
+    int low = 0;
+    int high = n1;
+    int n = n1 + n2;
+    int leftHalf = (n1 + n2 + 1) / 2; // for odd size also manage
+    while (low <= high)
+    {
+        int mid1 = (low + high) / 2;
+        int mid2 = leftHalf - mid1;
+
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX , r2 = INT_MAX;
+
+        if(mid1 < n1) r1 = nums1[mid1]; //r1 is valid so place else remain it INT_MAX
+        if(mid2 < n2) r2 = nums2[mid2];
+        if(mid1 - 1 >= 0) l1 = nums1[mid1-1]; 
+        if(mid2 - 1 >= 0) l2 = nums2[mid2-1]; 
+
+        // If we get valid symmerty
+        if(l1 < r2 && l2 < r1){
+            // odd size array
+            if(n%2 != 0) return  max(l1, l2);
+            return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+        }
+        else if(l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+
+    return 0.0;
+}
 int main()
 {
     vector<int> nums1 = {1, 2};
     vector<int> nums2 = {3, 4};
     cout << "find Median Sorted Arrays Brute : " << findMedianSortedArraysBrute(nums1, nums2) << endl;
     cout << "find Median Sorted Arrays Better : " << findMedianSortedArraysBetter(nums1, nums2) << endl;
+    cout << "find Median Sorted Arrays Optimal : " << findMedianSortedArraysOptimal(nums1, nums2) << endl;
 
     return 0;
 }
