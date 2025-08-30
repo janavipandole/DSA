cced2479b77d0f53132cea6d0543c8137250522a
Janavi Pandole <janavipandole@gmail.com>
2025-08-30 08:08:19 +0530

    Valid Sudoku

---
+++ Array/Valid-Sudoku.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool isValidSudoku(vector<vector<char>>& board) {
+    vector<unordered_set<char>> rows(9), cols(9), boxes(9);
+
+    for (int r = 0; r < 9; ++r) {
+        for (int c = 0; c < 9; ++c) {
+            char val = board[r][c];
+            if (val == '.') continue;
+
+            int boxIndex = (r / 3) * 3 + (c / 3);
+
+            if (rows[r].count(val) || cols[c].count(val) || boxes[boxIndex].count(val))
+                return false;
+
+            rows[r].insert(val);
+            cols[c].insert(val);
+            boxes[boxIndex].insert(val);
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    vector<vector<char>> board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                                  {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                                  {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                                  {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                                  {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                                  {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                                  {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                                  {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                                  {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+    cout << "is Valid Sudoku : " << isValidSudoku(board) << endl;
+    return 0;
+}
