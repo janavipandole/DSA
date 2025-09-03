d849c5d5529302f6f4d4e3d4f991eff535316f7f
Janavi Pandole <janavipandole@gmail.com>
2025-09-03 10:23:39 +0530

    First Bad Version Brute force solution is add

--- BinarySearch/First-Bad-Version.cpp
+++ BinarySearch/First-Bad-Version.cpp
@@ -5,10 +5,23 @@ using namespace std;
 
 bool isBadVersion(int num)
 {
-    if (num >= 4)return true;
+    if (num >= 4)
+        return true;
     return false;
 }
-int firstBadVersion(int n)
+// Brute force solution
+int firstBadVersionBrute(int n)
+{
+    int low = 1, high = n;
+    for(int i = low; i <= high; i++){
+        if(isBadVersion(i)){
+            return i;
+        }
+    }
+    return -1;
+}
+// Optimal solution
+int firstBadVersionOptimal(int n)
 {
     int low = 1, high = n;
 
@@ -16,13 +29,16 @@ int firstBadVersion(int n)
     {
         int mid = low + (high - low) / 2;
 
-        if (isBadVersion(mid)) high = mid - 1;
-        else low = mid + 1;
+        if (isBadVersion(mid))
+            high = mid - 1;
+        else
+            low = mid + 1;
     }
     return low;
 }
 int main()
 {
-    cout << "first Bad Version : " << firstBadVersion(5) << endl;
+    cout << "first Bad Version : " << firstBadVersionBrute(5) << endl;
+    cout << "first Bad Version : " << firstBadVersionOptimal(5) << endl;
     return 0;
 }
