4f6f2da38bc8f14d2de7682c357c05c83bea4247
Janavi Pandole <janavipandole@gmail.com>
2025-09-03 10:13:37 +0530

    Guess Number Higher or Lower

---
+++ BinarySearch/Guess-Number-Higher-or-Lower.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+int guess(int num)
+{
+    int n = 6;
+
+    if(n == num) return 0;
+    else if(n < num) return -1;
+    else return 1;
+}
+int guessNumber(int n)
+{
+    int low = 1, high = n;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (guess(mid) == 0) return mid;
+        if (guess(mid) == -1) high = mid - 1;
+        else low = mid + 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    cout << "guessNumber : " << guessNumber(10) << endl;
+    return 0;
+}
