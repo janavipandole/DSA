070ccc23b3b2f2962fbdf8b1890ab4e4ac36d96e
Janavi Pandole <janavipandole@gmail.com>
2025-09-09 07:59:09 +0530

    Count the Number of Consistent Strings Optimal solution is add

--- BitManipulation/Count-the-Number-of-Consistent-Strings.cpp
+++ BitManipulation/Count-the-Number-of-Consistent-Strings.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <unordered_set>
 using namespace std;
-int countConsistentStrings(string allowed, vector<string> &words)
+// Brute force solution
+int countConsistentStringsBrute(string allowed, vector<string> &words)
 {
     int cnt = 0;
     unordered_set<char> aset(allowed.begin(), allowed.end());
@@ -26,10 +27,42 @@ int countConsistentStrings(string allowed, vector<string> &words)
     return cnt;
 }
 
+// Optimal solution
+int countConsistentStringsOptimal(string allowed, vector<string> &words)
+{
+    int cnt = 0;
+    int mask = 0;
+
+    // Create bitmask for allowed characters
+    for (char ch : allowed)
+    {
+        mask |= (1 << (ch - 'a'));
+    }
+
+    for (const string &word : words)
+    {
+        bool isConsistent = true;
+        for (char ch : word)
+        {
+            if ((mask & (1 << (ch - 'a'))) == 0)
+            {
+                isConsistent = false;
+                break;
+            }
+        }
+        if (isConsistent)
+            cnt++;
+    }
+
+    return cnt;
+}
+
 int main()
 {
     string allowed = "ab";
     vector<string> words = {"ad", "bd", "aaab", "baa", "badab"};
-    cout << "Count the Number of Consistent Strings : " << countConsistentStrings(allowed, words) << endl;
+    cout << "Count the Number of Consistent Strings : " << countConsistentStringsBrute(allowed, words) << endl;
+    cout << "Count the Number of Consistent Strings : " << countConsistentStringsOptimal(allowed, words) << endl;
+
     return 0;
 }
