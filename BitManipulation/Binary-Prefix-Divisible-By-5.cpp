527c767892c01561e3807ffac8a94139c6732c4e
Janavi Pandole <janavipandole@gmail.com>
2025-09-11 11:41:02 +0530

    Binary Prefix Divisible By 5

---
+++ BitManipulation/Binary-Prefix-Divisible-By-5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+vector<bool> prefixesDivBy5(vector<int> &nums)
+{
+    int num = 0;
+    vector<bool> result(nums.size());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        num = (num * 2 + nums[i]) % 5;
+        if (num % 5 == 0)
+        {
+            result[i] = true;
+        }
+        else
+        {
+            result[i] = false;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> nums = {1, 1, 1};
+    cout << "Binary Prefix Divisible By 5 : " << endl;
+    vector<bool> ans = prefixesDivBy5(nums);
+
+    for (auto n : ans)
+    {
+        cout << n << " ";
+    }
+    cout << endl;
+    return 0;
+}
