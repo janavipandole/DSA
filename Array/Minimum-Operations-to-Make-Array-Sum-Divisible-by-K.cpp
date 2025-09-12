89f0e8835c3e772ec9a205246bb1fb48ed1517ed
Janavi Pandole <janavipandole@gmail.com>
2025-09-12 07:15:52 +0530

    Minimum Operations to Make Array Sum Divisible by K

---
+++ Array/Minimum-Operations-to-Make-Array-Sum-Divisible-by-K.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int minOperations(vector<int> &nums, int k)
+{
+    int sum = 0;
+    for (auto num : nums)
+    {
+        sum += num;
+    }
+
+    if (sum % k == 0)
+        return 0;
+    return sum % k;
+}
+int main()
+{
+    vector<int> nums = {4, 1, 3};
+    int k = 4;
+    cout << "Minimum Operations to Make Array Sum Divisible by K : " << minOperations(nums, k) << endl;
+    return 0;
+}
