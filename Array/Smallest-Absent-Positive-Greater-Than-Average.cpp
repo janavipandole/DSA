6e6d3191967190385964806ab82db1b7ebf6a747
Janavi Pandole <janavipandole@gmail.com>
2025-09-14 19:44:11 +0530

    Smallest Absent Positive Greater Than Average

---
+++ Array/Smallest-Absent-Positive-Greater-Than-Average.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int smallestAbsent(vector<int> &nums)
+{
+    vector<bool> seen(107, false);
+    int sum = 0;
+    for (int x : nums)
+    {
+        if (x >= 1)
+            seen[x] = true;
+        sum += x;
+    }
+
+    for (int ans = 1;; ans++)
+    {
+        if (seen[ans] || ans * (int)nums.size() <= sum)
+        {
+            continue;
+        }
+        return ans;
+    }
+}
+int main()
+{
+    vector<int> nums = {-1, 1, 2};
+    cout << "Smallest Absent Positive Greater Than Average : " << smallestAbsent(nums) << endl;
+    return 0;
+}
