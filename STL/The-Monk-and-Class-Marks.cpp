c2ad5971aea2033af0acce4ea1fb04c70b44cdb9
Janavi Pandole <janavipandole@gmail.com>
2025-09-22 07:30:40 +0530

    The Monk and Class Marks update

--- STL/The-Monk-and-Class-Marks.cpp
+++ STL/The-Monk-and-Class-Marks.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <map>
 using namespace std;
 int main()
 {
+    cout << "The Monk and Class Marks" << endl;
     int n;
     cin >> n;
-    map<int, vector<string>> mpp;
+    map<int, multiset<string>> mpp;
     while (n--)
     {
         string name;
         cin >> name;
         int mark;
         cin >> mark;
-        mpp[mark].push_back(name);
+        mpp[mark].insert(name);
     }
 
     for (auto info = mpp.rbegin(); info != mpp.rend(); info++)
     {
-        sort(info->second.begin(), info->second.end());
         for (auto name : info->second)
         {
             cout << name << " " << info->first << endl;
