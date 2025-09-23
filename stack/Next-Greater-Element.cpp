d3713cc932f4b262790e8bc0096c74a6585b5711
Janavi Pandole <janavipandole@gmail.com>
2025-09-23 07:21:51 +0530

    Next Greater Element

---
+++ stack/Next-Greater-Element.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void NGE(int n, vector<int> &nums)
+{
+    stack<int> s;
+    vector<pair<int, int>> ans(n);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!s.empty() && s.top() <= nums[i])
+        {
+            s.pop();
+        }
+
+        if (s.empty())
+        {
+            ans[i] = {nums[i], -1};
+        }
+        else
+        {
+            ans[i] = {nums[i], s.top()};
+        }
+
+        s.push(nums[i]);
+    }
+
+    for (auto &val : ans)
+    {
+        cout << val.first << " " << val.second << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        nums[i] = num;
+    }
+    NGE(n, nums);
+    return 0;
+}
