bc765eb21099b0e510e90764c8cfbc1805d703e5
Janavi Pandole <janavipandole@gmail.com>
2025-09-24 07:45:46 +0530

    Maximize Sum Of Array After K Negations

---
+++ Array/Maximize-Sum-Of-Array-After-K-Negations.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int largestSumAfterKNegations(vector<int> &nums, int k)
+{
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < k; i++)
+    {
+        nums[0] *= -1;
+        sort(nums.begin(), nums.end());
+    }
+    int sum = 0;
+    for (auto num : nums)
+    {
+        sum += num;
+    }
+    return sum;
+}
+int main()
+{
+    vector<int> nums = {3, -1, 0, 2};
+    int k = 3;
+    cout << "Maximize Sum Of Array After K Negations : " << largestSumAfterKNegations(nums, 1) << endl;
+    return 0;
+}
