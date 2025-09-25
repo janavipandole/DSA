eeb97bdc18e5c84c6da987ba311653f4c6d7fb32
Janavi Pandole <janavipandole@gmail.com>
2025-09-25 08:04:55 +0530

    Find the Number of Good Pairs I

---
+++ Array/Find-the-Number-of-Good-Pairs-I.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int numberOfPairs(vector<int> &nums1, vector<int> &nums2, int k)
+{
+    int countPairs = 0;
+    for (int i = 0; i < nums1.size(); i++)
+    {
+        for (int j = 0; j < nums2.size(); j++)
+        {
+            if (nums1[i] % (nums2[j] * k) == 0) countPairs++;
+        }
+    }
+    return countPairs;
+}
+int main()
+{
+    vector<int> nums1 = {1,3,4}, nums2 = {1,3,4};
+    int k = 1;
+    cout<<"Find the Number of Good Pairs I : " <<numberOfPairs(nums1, nums2, k)<<endl;
+    return 0;
+}
