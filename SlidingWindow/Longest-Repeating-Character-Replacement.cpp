7396c0fd08724ffdb9102d1c8301a5c5fedf3ba4
Janavi Pandole <janavipandole@gmail.com>
2025-10-15 08:16:42 +0530

    Longest Repeating Character Replacement

--- SlidingWindow/Longest-Repeating-Character-Replacement.cpp
+++ SlidingWindow/Longest-Repeating-Character-Replacement.cpp
@@ -10,23 +10,62 @@ public:
         int maxLen = 0;
         for (int i = 0; i < s.size(); i++)
         {
-            int cnt = 0;
+            vector<int> mpp(26, 0);
+            int maxFreq = 0, change = 0;
             for (int j = i; j < s.size(); j++)
             {
-                if (s[i] != s[j]) cnt++;
-                if (cnt > k) break;
-                maxLen = max(maxLen, j - i + 1);
+                maxFreq = max(maxFreq, ++mpp[s[j] - 'A']);
+                change = (j - i + 1) - maxFreq;
+                if (change <= k) maxLen = max(maxLen, j - i + 1);
+                else break;
             }
         }
         return maxLen;
     }
-   
+    int characterReplacement_Better(string s, int k)
+    {
+        int maxLen = 0, st = 0, end = 0, maxFreq = 0;
+        vector<int> mpp(26, 0);
+        while (end < s.size())
+        {
+            maxFreq = max(maxFreq, ++mpp[s[end] - 'A']);
+            while ((end - st + 1) - maxFreq > k)
+            {
+                mpp[s[st] - 'A']--;
+                st++;
+            }
+            maxLen = max(maxLen, end - st + 1);
+            end++;
+        }
+        return maxLen;
+    }
+    // Optimal solution
+    int characterReplacement_Optimal(string s, int k)
+    {
+        int maxLen = 0, st = 0, end = 0, maxFreq = 0;
+        vector<int> mpp(26, 0);
+        while (end < s.size())
+        {
+            maxFreq = max(maxFreq, ++mpp[s[end] - 'A']);
+            if ((end - st + 1) - maxFreq > k)
+            {
+                mpp[s[st] - 'A']--;
+                st++;
+            }
+            maxLen = max(maxLen, end - st + 1);
+            end++;
+        }
+        return maxLen;
+    }
 };
 int main()
 {
     Solution s;
-    string str = "ABAA";
-    int k = 0;
+    string str = "AABABBA";
+    int k = 1;
     cout << "Longest Repeating Character Replacement : " << s.characterReplacement_Brute(str, k) << endl;
+    cout << "Longest Repeating Character Replacement : " << s.characterReplacement_Better(str, k) << endl;
+    cout << "Longest Repeating Character Replacement : " << s.characterReplacement_Optimal(str, k) << endl;
+
     return 0;
 }
