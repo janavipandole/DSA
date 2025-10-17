94cdd49bf61ed33a94289ce05e04db2019449666
Janavi Pandole <janavipandole@gmail.com>
2025-10-17 06:35:31 +0530

    Binary Subarrays With Sum Better Solution is added

--- SlidingWindow/Binary-Subarrays-With-Sum.cpp
+++ SlidingWindow/Binary-Subarrays-With-Sum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 class Solution
@@ -15,15 +16,32 @@ public:
             {
                 sum += nums[j];
                 if (sum == k) cnt++;
-                if (sum > k)  break;
+                if (sum > k) break;
             }
         }
         return cnt;
     }
-    // Optimal Approch
-    int lessThenEqualToGoal(vector<int> &nums, int goal)
+    // Better Approch
+    int numSubarraysWithSum_Better(vector<int> &nums, int k)
     {
-        if(goal < 0) return 0;
+        int cnt = 0;
+        int sum = 0;
+        unordered_map<int, int> m;
+        m[0] = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            int remove = sum - k;
+            cnt += m[remove];
+            m[sum] += 1;
+        }
+        return cnt;
+    }
+    // Optimal Approch
+    int helper(vector<int> &nums, int goal)
+    {
+        if (goal < 0)
+            return 0;
         int cnt = 0, end = 0, st = 0, n = nums.size(), sum = 0;
         while (end < n)
         {
@@ -35,16 +53,16 @@ public:
                 st++;
             }
 
-            if (sum <= goal)cnt += end - st + 1;
+            if (sum <= goal) cnt += end - st + 1;
 
             end++;
         }
         return cnt;
     }
-    
+
     int numSubarraysWithSum_Optimal(vector<int> &nums, int k)
     {
-        return lessThenEqualToGoal(nums, k) - lessThenEqualToGoal(nums, k-1);
+        return helper(nums, k) - helper(nums, k - 1);
     }
 };
 int main()
@@ -54,6 +72,7 @@ int main()
     int goal = 2;
 
     cout << "Binary Subarrays With Sum : " << s.numSubarraysWithSum_Brute(nums, goal) << endl;
+    cout << "Binary Subarrays With Sum : " << s.numSubarraysWithSum_Better(nums, goal) << endl;
     cout << "Binary Subarrays With Sum : " << s.numSubarraysWithSum_Optimal(nums, goal) << endl;
 
     return 0;
