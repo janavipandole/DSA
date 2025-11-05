84f7e18a93c4dd0a95d730aed8b6d95f91632718
Janavi Pandole <janavipandole@gmail.com>
2025-11-05 07:58:45 +0530

    Candy

---
+++ GreedyApproch/Candy.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    int candy_1(vector<int> &ratings)
+    {
+        int n = ratings.size();
+        int sum = 0;
+        vector<int> left(n, 1), right(n, 1);
+
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                left[i] = left[i - 1] + 1;
+            }
+            else
+            {
+                left[i] = 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1])
+            {
+                right[i] = right[i + 1] + 1;
+            }
+            else
+            {
+                right[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += max(right[i], left[i]);
+        }
+        return sum;
+    }
+
+    int candy_2(vector<int> &ratings)
+    {
+        int n = ratings.size();
+        int sum = 0, right = 1;
+        vector<int> left(n, 1);
+
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                left[i] = left[i - 1] + 1;
+            }
+            else
+            {
+                left[i] = 1;
+            }
+        }
+        sum = left[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1])
+            {
+                right++;
+            }
+            else
+            {
+                right = 1;
+            }
+            sum += max(left[i], right);
+        }
+
+        return sum;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> ratings = {1, 3, 2, 2, 1};
+    cout << "Candy : " << s.candy_1(ratings) << endl;
+    cout << "Candy : " << s.candy_2(ratings) << endl;
+
+    return 0;
+}
