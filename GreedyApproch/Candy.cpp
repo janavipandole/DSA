14a4e8b8515f205d0b8b100b07f24cff018e7393
Janavi Pandole <janavipandole@gmail.com>
2025-11-06 10:27:44 +0530

    Candy update optimal solution

--- GreedyApproch/Candy.cpp
+++ GreedyApproch/Candy.cpp
@@ -62,6 +62,36 @@ public:
 
         return sum;
     }
+
+    int candy_3(vector<int> &ratings)
+    {
+        int n = ratings.size();
+        int sum = 1;
+        int i = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]) {
+                sum += 1;
+                i++;
+                continue;;
+            }
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
+            }
+
+            int down = 0;
+            while(i < n && ratings[i] < ratings[i-1]){
+                down++;
+                sum += down;
+                i++;
+            }
+            down++;
+            if(down > peak) sum += (down - peak);
+        }
+        return sum;
+    }
 };
 int main()
 {
@@ -69,6 +99,7 @@ int main()
     vector<int> ratings = {1, 3, 2, 2, 1};
     cout << "Candy : " << s.candy_1(ratings) << endl;
     cout << "Candy : " << s.candy_2(ratings) << endl;
+    cout << "Candy : " << s.candy_3(ratings) << endl;
 
     return 0;
 }
