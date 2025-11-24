750c6a8f1562d0630f6a31b517c25b030d1e9353
Janavi Pandole <janavipandole@gmail.com>
2025-11-24 07:44:39 +0530

    Insert Node befor the Head of the DLL

---
+++ LinkList/InserNode-after-the-Head-of-the-DLL.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = prev = NULL;
+    }
+};
+
+class DoublyList
+{
+public:
+    Node *head;
+    DoublyList()
+    {
+        head = NULL;
+    }
+    Node *InsertAfterTheHead(Node *head, int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            return head;
+        }
+
+        Node *temp = head->next;
+
+        head->next = newNode;
+        newNode->prev = head;
+
+        if (temp)
+        {
+            newNode->next = temp;
+            temp->prev = newNode;
+        }
+        return head;
+    }
+
+    void printDll()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << " <=> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+};
+
+int main()
+{
+    DoublyList DLL;
+    cout << "Insert Node befor the Head of the DLL : " << endl;
+    DLL.head = DLL.InsertAfterTheHead(DLL.head, 1);
+    DLL.head = DLL.InsertAfterTheHead(DLL.head, 2);
+    DLL.head = DLL.InsertAfterTheHead(DLL.head, 3);
+    DLL.head = DLL.InsertAfterTheHead(DLL.head, 4);
+    DLL.printDll();
+    return 0;
+}
