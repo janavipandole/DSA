b6ff1804f62f0b28f4d9eb085945d1bd5601f725
Janavi Pandole <janavipandole@gmail.com>
2025-11-24 07:50:17 +0530

    Insert Node After the Head of the DLL

---
+++ LinkList/Insert-Node-after-the-last-of-the-DLL.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = prev = NULL;
+    }
+};
+
+class DoublyList
+{
+public:
+    Node *head;
+    DoublyList()
+    {
+        head = NULL;
+    }
+    Node *InsertAfterTheLastNode(Node *head, int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            return head;
+        }
+
+        Node *temp = head;
+
+        while (temp->next)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+        newNode->prev = temp;
+
+        return head;
+    }
+
+    void printDll()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << " <=> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+};
+
+int main()
+{
+    DoublyList DLL;
+    cout << "Insert Node After the Head of the DLL : " << endl;
+    DLL.head = DLL.InsertAfterTheLastNode(DLL.head, 1);
+    DLL.head = DLL.InsertAfterTheLastNode(DLL.head, 2);
+    DLL.head = DLL.InsertAfterTheLastNode(DLL.head, 3);
+    DLL.head = DLL.InsertAfterTheLastNode(DLL.head, 4);
+    DLL.head = DLL.InsertAfterTheLastNode(DLL.head, 5);
+
+    DLL.printDll();
+    return 0;
+}
