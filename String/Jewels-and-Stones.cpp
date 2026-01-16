254b21f5244cfa67d98ea9106065bf449efd3098
Janavi Pandole <janavipandole@gmail.com>
2026-01-16 18:09:57 +0530

    Jewels and Stones

--- String/Jewels-and-Stones.cpp
+++ String/Jewels-and-Stones.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int numJewelsInStones(string jewels, string stones)
+class Solution
 {
-    unordered_map<char, bool> mpp;
-    int result = 0;
-    for (auto jewel : jewels)
+public:
+    int numJewelsInStones(string jewels, string stones)
     {
-        mpp[jewel] = true;
-    }
+        unordered_map<char, bool> mpp;
+        int result = 0;
+        for (auto jewel : jewels)
+        {
+            mpp[jewel] = true;
+        }
 
-    for(auto stone : stones){
-        if(mpp.find(stone) != mpp.end()) result += 1;
+        for (auto stone : stones)
+        {
+            if (mpp.find(stone) != mpp.end())
+                result += 1;
+        }
+        return result;
     }
-    return result;
-}
+};
 int main()
 {
+    Solution s;
     string jewels = "aA", stones = "aAAbbbb";
+    cout << "Jewels and Stones : " << s.numJewelsInStones(jewels, stones) << endl;
     return 0;
 }
