c7a2af9d2c56a058751ac5bbd9ce027b28b59f7d
Janavi Pandole <janavipandole@gmail.com>
2026-03-08 10:30:11 +0530

    Implement Queues using Stack

--- Queue/Implement-Queue-using-Stacks.cpp
+++ Queue/Implement-Queue-using-Stacks.cpp
@@ -21,27 +21,59 @@ public:
 
     void push(int x)
     {
+        if (st1.empty())
+        {
+            st1.push(x);
+            return ;
+        }
+        while (!st1.empty())
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+
+        st1.push(x);
+
+        while (!st2.empty())
+        {
+            st1.push(st2.top());
+            st2.pop();
+        }
     }
 
     int pop()
     {
+        int ans = st1.top();
+        st1.pop();
+        return ans;
     }
 
     int peek()
     {
+        int ans = st1.top();
+        return ans;
     }
 
     bool empty()
     {
+        return st1.empty();
     }
 };
 
 int main()
 {
-    MyQueue *obj = new MyQueue();
-    obj->push(2);
-    int param_2 = obj->pop();
-    int param_3 = obj->peek();
-    bool param_4 = obj->empty();
+    MyQueue *qu = new MyQueue();
+    qu->push(1);
+    qu->push(2);
+    qu->push(3);
+    qu->push(4);
+
+    cout << "Implement Queues using Stack : " << endl;
+    while (!qu->empty())
+    {
+        cout << qu->peek() << " ";
+        qu->pop();
+    }
+    cout << endl;
     return 0;
 }
