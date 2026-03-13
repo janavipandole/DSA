30ecff3f26640acf7ffd6bee8af3b53c3ad14a89
Janavi Pandole <janavipandole@gmail.com>
2026-03-13 13:17:05 +0530

    Postfix to Infix Conversion

---
+++ stack/Postfix-to-Infix.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+// User function Template for C++
+class Solution
+{
+public:
+    string postToInfix(string &exp)
+    {
+        stack<string> ans;
+        int i = 0, n = exp.length();
+        while (i < n)
+        {
+            if (isalnum(exp[i]))
+            {
+                string s(1, exp[i]);
+                ans.push(s);
+            }
+            else
+            {
+                string s1 = ans.top();
+                ans.pop();
+
+                string s2 = ans.top();
+                ans.pop();
+
+                string newS = "(" + s2 + string(1, exp[i]) + s1 + ")";
+                ans.push(newS);
+            }
+            i++;
+        }
+        return ans.top();
+    }
+};
+int main()
+{
+    Solution s;
+    string str = "ab*c+";
+    cout << "Postfix to Infix Conversion : " << s.postToInfix(str) << endl;
+
+    return 0;
+}
